6b9b21f184531a23dbc737d536e69f1dd7b0ee9b
hakhyun0615 <hakhyun0615@naver.com>
2025-02-18 11:42:21 +0900

    main

---
+++ code/utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def highlight_print(text):
+    print('\033[93m' + str(text) + '\033[0m')
+
+def save_memory(df):
+    cols = df.dtypes.index.tolist()
+    types = df.dtypes.values.tolist()
+    for i,t in enumerate(types):
+        if 'int' in str(t):
+            if df[cols[i]].min() > np.iinfo(np.int8).min and df[cols[i]].max() < np.iinfo(np.int8).max:
+                df[cols[i]] = df[cols[i]].astype(np.int8)
+            elif df[cols[i]].min() > np.iinfo(np.int16).min and df[cols[i]].max() < np.iinfo(np.int16).max:
+                df[cols[i]] = df[cols[i]].astype(np.int16)
+            elif df[cols[i]].min() > np.iinfo(np.int32).min and df[cols[i]].max() < np.iinfo(np.int32).max:
+                df[cols[i]] = df[cols[i]].astype(np.int32)
+            else:
+                df[cols[i]] = df[cols[i]].astype(np.int64)
+        elif 'float' in str(t):
+            if df[cols[i]].min() > np.finfo(np.float16).min and df[cols[i]].max() < np.finfo(np.float16).max:
+                df[cols[i]] = df[cols[i]].astype(np.float16)
+            elif df[cols[i]].min() > np.finfo(np.float32).min and df[cols[i]].max() < np.finfo(np.float32).max:
+                df[cols[i]] = df[cols[i]].astype(np.float32)
+            else:
+                df[cols[i]] = df[cols[i]].astype(np.float64)
+    return df  
