6f5afd624b3a0a6d3f235db178b6c7de747a0413
hakhyun0615 <hakhyun0615@naver.com>
2025-02-20 02:32:36 +0900

    main

--- README.md
+++
@@ -1,2 +0,0 @@
-# SCI Journal 2
-This repository contains the source code for the SCI Journal 2.
--- code/utils.py
+++ code/utils.py
@@ -1,26 +1,80 @@
+import psutil
+import GPUtil
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 def highlight_print(text):
     print('\033[93m' + str(text) + '\033[0m')
 
-def save_memory(df):
-    cols = df.dtypes.index.tolist()
-    types = df.dtypes.values.tolist()
-    for i,t in enumerate(types):
-        if 'int' in str(t):
-            if df[cols[i]].min() > np.iinfo(np.int8).min and df[cols[i]].max() < np.iinfo(np.int8).max:
-                df[cols[i]] = df[cols[i]].astype(np.int8)
-            elif df[cols[i]].min() > np.iinfo(np.int16).min and df[cols[i]].max() < np.iinfo(np.int16).max:
-                df[cols[i]] = df[cols[i]].astype(np.int16)
-            elif df[cols[i]].min() > np.iinfo(np.int32).min and df[cols[i]].max() < np.iinfo(np.int32).max:
-                df[cols[i]] = df[cols[i]].astype(np.int32)
+def reduce_memory(df, verbose=False):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
             else:
-                df[cols[i]] = df[cols[i]].astype(np.int64)
-        elif 'float' in str(t):
-            if df[cols[i]].min() > np.finfo(np.float16).min and df[cols[i]].max() < np.finfo(np.float16).max:
-                df[cols[i]] = df[cols[i]].astype(np.float16)
-            elif df[cols[i]].min() > np.finfo(np.float32).min and df[cols[i]].max() < np.finfo(np.float32).max:
-                df[cols[i]] = df[cols[i]].astype(np.float32)
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Memory usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+def get_optimal_num_batches(gpu_available):
+    # memory
+    memory_info = psutil.virtual_memory()
+    total_memory = memory_info.total
+    available_memory = memory_info.available
+
+    # cpu
+    cpu_usage = psutil.cpu_percent(interval=None)
+
+    # gpu
+    if gpu_available:
+        try:
+            gpus = GPUtil.getGPUs()
+            if gpus:
+                gpu_memory = sum(gpu.memoryTotal for gpu in gpus)
+                gpu_memory_available = sum(gpu.memoryFree for gpu in gpus)
             else:
-                df[cols[i]] = df[cols[i]].astype(np.float64)
-    return df  
+                gpu_memory = 0
+                gpu_memory_available = 0
+        except ImportError:
+            gpu_memory = 0
+            gpu_memory_available = 0
+    else:
+        gpu_memory = 0
+        gpu_memory_available = 0
+
+    # optimal
+    memory_factor = available_memory / total_memory
+    cpu_factor = (100 - cpu_usage) / 100
+    gpu_factor = gpu_memory_available / max(gpu_memory, 1) if gpu_available else 1
+    optimal_num_batches = int(200 * (0.5 * memory_factor + 0.3 * cpu_factor + 0.2 * gpu_factor))
+
+    print(f"Optimal num batches: {optimal_num_batches}")
+    
+    return max(1, optimal_num_batches)
+
+
+# plt.figure(figsize=(10, 5))
+# plt.plot(tests[idx][-100:].to_timestamp(), label="Actual")
+# plt.plot(pd.Series(forecasts[idx].quantile(0.5), index=forecasts[idx].start_date.to_timestamp() + pd.to_timedelta(range(len(forecasts[idx].quantile(0.5))), unit='D')), label="Forecast")
+# plt.title(f"Forecast for Time Series {idx + 1}")
+# plt.legend(loc="upper right")
+# plt.show()
