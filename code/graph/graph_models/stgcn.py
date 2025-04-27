0400b893acfb2cbd1bc0c7baf6a77092876f1532
KimHakHyun <hakhyun0615@naver.com>
2025-04-28 03:16:53 +0900

    main

--- code/graph/graph_models/stgcn.py
+++ code/graph/graph_models/stgcn.py
@@ -10,8 +10,8 @@ class TemporalConv(nn.Module):
         in_channels: feature_dim
         out_channels: num_channels
         kernel
-            W: 1 (every node uses seperate filters independently)
-            H: kernel_size
+            H: 1 (every node uses seperate filter (in_channels, 1, kernel_size) independently)
+            W: kernel_size
         """
         self.conv_1 = nn.Conv2d(in_channels, out_channels, (1, kernel_size)) # main information
         self.conv_2 = nn.Conv2d(in_channels, out_channels, (1, kernel_size)) # gate
@@ -19,12 +19,11 @@ class TemporalConv(nn.Module):
     def forward(self, X: torch.FloatTensor) -> torch.FloatTensor:
         r"""
         input 
-            X: (batch_size, time_steps, num_nodes, feature_dim)
+            X: (batch_size, feature_dim, num_nodes, window_size)
         output 
-            H: (batch_size, time_steps-kernel_size+1, num_nodes, num_channels)
+            H: (batch_size, num_channels, num_nodes, window_size-kernel_size+1)
         """
-        X = X.permute(0, 3, 2, 1) # batch_size, feature_dim (C), num_nodes (W), time_steps (H))
-        P = self.conv_1(X) # (batch_size, num_channels, num_nodes, time_steps-kernel_size+1)
+        P = self.conv_1(X) # (batch_size, num_channels, num_nodes, window_size-kernel_size+1)
         Q = torch.sigmoid(self.conv_2(X))
         PQ = torch.mul(P, Q) # GLU (element-wise multiplication of P, Q): selective information flow
         return PQ
@@ -41,14 +40,7 @@ class STConv(nn.Module):
         K: int
     ):
         super(STConv, self).__init__()
-        r"""
-        TemporalConv
-            input: (batch_size, time_steps, num_nodes, feature_dim)
-            output: (batch_size, time_steps-kernel_size+1, num_nodes, num_filters)
-        ChebConv: 
-            input: (batch_size, num_nodes, feature_dim)
-            output: (batch_size, num_nodes, num_channels)
-        """
+
         self.num_nodes = num_nodes
         self.feature_dim = feature_dim
         self.num_temporal1_channels = num_temporal1_channels
@@ -77,12 +69,13 @@ class STConv(nn.Module):
     def forward(self, X: torch.FloatTensor, edge_index: torch.LongTensor) -> torch.FloatTensor:
         r"""
         intput 
-            X: (batch_size, time_steps, num_nodes, feature_dim)
+            X: (batch_size, feature_dim, num_nodes, window_size)
             edge_index: (2, num_edges)
         output
-            T: (batch_size, time_steps-2*(kernel_size-1), num_nodes, num_temporal2_channels)
+            T: (batch_size, num_temporal2_channels, num_nodes, window_size-2*(kernel_size+1))
         """
-        T_0 = self._temporal_conv1(X) # (batch_size, time_steps-kernel_size+1, num_nodes, num_temporal1_channels)
+        T_0 = self._temporal_conv1(X) # (batch_size, num_temporal1_channels, num_nodes, window_size-kernel_size+1)
+        T_0 = T_0.permute(0, 3, 2, 1) # (batch_size, window_size-kernel_size+1, num_nodes, num_temporal1_channels)
         T = torch.zeros(
             T_0.size(0),
             T_0.size(1),
@@ -90,14 +83,15 @@ class STConv(nn.Module):
             self.num_spatial_channels,
         ).to(T_0.device)
         for b in range(T_0.size(0)): # batch_size
-            for t in range(T_0.size(1)): # time_steps-kernel_size+1
+            for t in range(T_0.size(1)): # window_size-kernel_size+1
                 T[b][t] = self._graph_conv(T_0[b][t], edge_index) # (num_nodes, num_spatial_channels)
 
-        T = F.relu(T) # (batch_size, time_steps-kernel_size+1, num_nodes, num_spatial_channels)
-        T = self._temporal_conv2(T) # (batch_size, time_steps-2*(kernel_size+1), num_nodes, num_temporal2_channels)
-        T = T.permute(0, 2, 1, 3) # (batch_size (B), num_nodes (C), time_steps-2*(kernel_size+1) (H), num_temporal2_channels (W))
+        T = F.relu(T) # (batch_size, window_size-kernel_size+1, num_nodes, num_spatial_channels)
+        T = T.permute(0, 3, 2, 1)  # (batch_size, num_spatial_channels, num_nodes, window_size-kernel_size+1)
+        T = self._temporal_conv2(T) # (batch_size, num_temporal2_channels, num_nodes, window_size-2*(kernel_size+1))
+        T = T.permute(0, 2, 1, 3) # (batch_size (B), num_nodes (C), num_temporal2_channels (H), window_size-2*(kernel_size+1) (W))
         T = self._batch_norm(T) # normalize by C
-        T = T.permute(0, 2, 1, 3) # (batch_size, time_steps-2*(kernel_size+1), num_nodes, num_temporal2_channels)
+        T = T.permute(0, 2, 1, 3) # (batch_size, num_temporal2_channels, num_nodes, window_size-2*(kernel_size+1))
         return T
     
 class STGCN(nn.Module):
@@ -113,8 +107,7 @@ class STGCN(nn.Module):
     ):
         super(STGCN, self).__init__()
         
-        self.stconv_blocks = nn.ModuleList()
-        self.stconv_blocks.append(
+        self.stconv_blocks = nn.ModuleList([
             # first STConv block
             STConv(
                 num_nodes=num_nodes,
@@ -125,6 +118,7 @@ class STGCN(nn.Module):
                 kernel_size=kernel_size,
                 K=K,
             ),
+
             # second STConv block
             STConv(
                 num_nodes=num_nodes,
@@ -135,8 +129,7 @@ class STGCN(nn.Module):
                 kernel_size=kernel_size,
                 K=K,
             )
-        )
-
+        ])
         # align time dimension
         self.fc1= nn.Linear(
             in_features=28-2*(kernel_size+1),
@@ -152,15 +145,14 @@ class STGCN(nn.Module):
     def forward(self, X, edge_index):
         """
         input
-            X: (batch_size, time_steps, num_nodes, feature_dim)
+            X: (batch_size, feature_dim, num_nodes, window_size)
             edge_index: (2, num_edges)
         output
             X: (batch_size, num_nodes, 28)
         """
-        time_steps = X.size(1)
         for stconv in self.stconv_blocks:
-            X = stconv(X, edge_index) # (batch_size, time_steps-2*(kernel_size+1), num_nodes, num_temporal2_channels)
-        X = X.permute(0, 2, 3, 1)  # (batch_size, num_nodes, num_temporal2_channels, time_steps-2*(kernel_size+1))    
+            X = stconv(X, edge_index) # (batch_size, num_temporal2_channels, num_nodes, window_size-2*(kernel_size+1))
+        X = X.permute(0, 2, 1, 3)  # (batch_size, num_nodes, num_temporal2_channels, window_size-2*(kernel_size+1))    
         X = self.fc1(X)  # (batch, num_nodes, num_temporal2_channels, 28)
         X = self.relu(X)
         X = X.permute(0, 1, 3, 2) # (batch, num_nodes, 28, num_temporal2_channels)
